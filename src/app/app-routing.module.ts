fef20a69e5a222ee333b4bf31eda2ba15b956d16
DylanSer <dydouldy@gmail.com>
2022-05-02 15:50:57 +0200

    Equipe

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreationequipesComponent } from './creationequipes/creationequipes.component';
+import { EquipesComponent } from './equipes/equipes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{component: EquipesComponent, path: "equipes"},
+{component: CreationequipesComponent, path: "creationequipes"}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
