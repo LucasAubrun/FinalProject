fca0734fad4b416b9c62bc85c49b2a4e532427c8
Perroch <alexisperrocheau@orange.fr>
2022-05-02 15:53:53 +0200

    aaa

--- src/app/creation-evenement/creation-evenement.component.ts
+++ src/app/creation-evenement/creation-evenement.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-creation-evenement',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreationEvenementComponent implements OnInit {
 
-  constructor() { }
+  baseURL: string = "http://localhost:8282/";
+  result: string = "";
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
   }
 
+
+  creerEvent(val: any) {
+    let event = {
+      nom: val.nom,
+      date: val.date,
+      adresse: val.adresse,
+      recurrence: val.recurrence,
+      niveau: val.niveau,
+      description: val.decription,
+      nbmin: val.nbmin,
+      nbmax: val.nbmax,
+      activite: val.activite,
+    };
+    console.log(event);
+    this.http.post(this.baseURL + "event/save", event)
+      .subscribe({
+        next: (data) => { this.result = "Creation réussie" },
+        error: (err) => { console.log(err) }
+      });
+    ;
+  }
 }
