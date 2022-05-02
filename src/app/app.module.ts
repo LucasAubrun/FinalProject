d66c4fe311c9f43af1f870ef50a7161e81006761
raisonclara <raison.clara@gmail.com>
2022-05-02 15:11:26 +0200

    newactivite

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ActivitesComponent } from './activites/activites.component';
+import { NewactiviteComponent } from './newactivite/newactivite.component';
+import { FormsModule } from '@angular/forms';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ActivitesComponent
+    ActivitesComponent,
+    NewactiviteComponent
   ],
   imports: [
+
+    HttpClientModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/newactivite/newactivite.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
+
+@Component({
+  selector: 'app-newactivite',
+  templateUrl: './newactivite.component.html',
+  styleUrls: ['./newactivite.component.css']
+})
+export class NewactiviteComponent implements OnInit {
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+  }
+
+  createNewActivite(val: any) {
+    let activite = {
+      description: val.contenu,
+      nom: val.nom,
+      nbJoueurs: val.nbjoueurs,
+      type: val.type
+    }
+    this.http.post('http://localhost:8080/activites/save', activite)
+      .subscribe({
+        next: (data) => { },
+        error: (err) => { console.log(err) }
+      })
+    console.log(activite)
+  }
+
+}
