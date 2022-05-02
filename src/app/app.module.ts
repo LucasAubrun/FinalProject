5ca680a91143ef3308ef738bc1d33624c4f3558d
raisonclara <raison.clara@gmail.com>
2022-05-02 11:00:58 +0200

    activites

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ActivitesComponent } from './activites/activites.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ActivitesComponent
   ],
   imports: [
     BrowserModule,
