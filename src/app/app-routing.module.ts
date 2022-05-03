6f2b62092402ec2fb424b810569a61adafc8e9a0
LucasAubrun <lucas.aubrun@orange.fr>
2022-05-03 09:15:05 +0200

    Links connexion/inscription

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ActivitesComponent } from './activites/activites.component';
+import { ConnexionComponent } from './connexion/connexion.component';
 import { HomeComponent } from './home/home.component';
+import { InscriptionComponent } from './inscription/inscription.component';
 
 //import { CreationequipesComponent } from './prive/prive.component';
 //import { PubliqueComponent } from './publique/publique.component';
@@ -9,6 +11,8 @@ import { HomeComponent } from './home/home.component';
 const routes: Routes = [
 {component: ActivitesComponent, path: 'activites'},
 {component: HomeComponent, path: 'home'},
+{component: ConnexionComponent, path: 'connexion'},
+{component: InscriptionComponent, path: 'inscription'}
 //{component: PriveComponent, path: 'prive'},
 //{component: PubliqueComponent, path: 'publique'}
 
