04b7ea06a2fa0767cec8236411d3d741028105a4
DylanSer <dydouldy@gmail.com>
2022-05-04 14:44:46 +0200

    Equipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { HttpClientModule } from '@angular/common/http';
-import {MatButtonModule} from '@angular/material/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -53,8 +52,7 @@ import { ConnexionadminComponent } from './connexionadmin/connexionadmin.compone
     HttpClientModule,
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
-    MatButtonModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
