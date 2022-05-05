6b1dd6389e76e492c805d56a76081c5668129964
Longevillois <alexandre.roux55@gmail.com>
2022-05-05 09:55:22 +0200

    ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,6 +9,7 @@ import { InscriptionComponent } from './inscription/inscription.component';
 import { EvenementsComponent } from './evenements/evenements.component';
 import { MesevenementsComponent } from './mesevenements/mesevenements.component';
 import { CreationEvenementComponent } from './creation-evenement/creation-evenement.component';
+import { ClassementComponent } from './classement/classement.component';
 
 
 const routes: Routes = [
@@ -18,6 +19,7 @@ const routes: Routes = [
   { component: HomeComponent, path: 'home' },
   { component: ConnexionComponent, path: 'connexion' },
   { component: InscriptionComponent, path: 'inscription' },
+  {component: ClassementComponent, path: 'classement'},
   { path: '', redirectTo: '/connexion', pathMatch: 'full' },
   { component: EvenementsComponent, path: "evenement" },
   { component: MesevenementsComponent, path: 'mesevenements' },
